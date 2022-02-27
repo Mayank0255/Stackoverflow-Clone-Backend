644bfa71a25c9b32a3c7e97b9718b138d620703a
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-02-28 00:27:34 +0530

    deploy test

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ extended: true }));
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
 //
-//   app.get('*', (req, res) => {
+//   app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //   });
 // }
