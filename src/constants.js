b30fec72aba9dcddfbd29a5159e961c394197f97
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2022-03-03 23:48:04 +0530

    fix: add tags description bug

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://api.stackexchange.com/2.2';
+const API_BASE_URL = 'https://api.stackexchange.com/2.3';
 
 const GRAVATAR_URL = (index) => `https://secure.gravatar.com/avatar/${index}?s=164&d=identicon`;
 
